455868ec06c39c28b881021a46541d4be01b7e1b
Kotaro Arimura <kotaro_arimura@voyagegroup.com>
2017-04-18 16:55:08 +0900

    Initial Commit

---
+++ SwiftApiIntro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftApiIntro
+//
+//  Created by 有村 皓太郎 on 2017/04/18.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
