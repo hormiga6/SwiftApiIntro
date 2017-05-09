ce91632cc80a4244be5662d3aa23df657312c41d
Kotaro Arimura <kotaro_arimura@voyagegroup.com>
2017-05-09 19:50:28 +0900

    add empty json test case

--- SwiftApiIntroTests/JsonTest.swift
+++ SwiftApiIntroTests/JsonTest.swift
@@ -10,12 +10,24 @@ import XCTest
 import Foundation
 
 class JsonTest: XCTestCase {
-    
-    func testExample() {
-        let data =  "{\"hoge\": \"hogehoge\"}".data(using: String.Encoding.utf8, allowLossyConversion: false)!
-        let json = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: AnyObject]
-        XCTAssertEqual(String(describing: type(of: json)), "Dictionary<String, AnyObject>")
-        
-        XCTAssertEqual(json["hoge"] as! String, "hogehoge")
-    }
+
+  func testSimpleJson() {
+    let data =  "{\"hoge\": \"hogehoge\"}".data(using: String.Encoding.utf8, allowLossyConversion: false)!
+    let json = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: AnyObject]
+    XCTAssertEqual(String(describing: type(of: json)), "Dictionary<String, AnyObject>")
+
+    XCTAssertEqual(json["hoge"] as! String, "hogehoge")
+  }
+
+  func testEmptyJson() {
+    let data =  "{\"hoge\": {}}".data(using: String.Encoding.utf8, allowLossyConversion: false)!
+    let json = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: AnyObject]
+    XCTAssertEqual(String(describing: type(of: json)), "Dictionary<String, AnyObject>")
+    let dic = json["hoge"] as! Dictionary<String, AnyObject>
+    XCTAssertEqual(String(describing: type(of: dic)), "Dictionary<String, AnyObject>")
+    XCTAssertEqual(dic.count, 0)
+
+    //die
+    //XCTAssertEqual(String(describing: type(of: (json["hoge"] as! String))), "Dictionary<String, AnyObject>")
+  }
 }
