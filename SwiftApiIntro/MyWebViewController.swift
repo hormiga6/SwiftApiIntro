ee30047d9366a11c8928f2c360f8f4eb6f19e51d
Kotaro Arimura <kotaro_arimura@voyagegroup.com>
2017-06-07 17:32:50 +0900

    load self signed cert on web view

--- SwiftApiIntro/MyWebViewController.swift
+++ SwiftApiIntro/MyWebViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class MyWebViewController: UIViewController {
+class MyWebViewController: UIViewController, UIWebViewDelegate, NSURLConnectionDelegate, NSURLConnectionDataDelegate {
 
   @IBOutlet weak var webView: UIWebView!
 
@@ -17,7 +17,43 @@ class MyWebViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    let req = URLRequest(url: URL(string: "https://google.com")!)
+    webView.delegate = self
+    let req = URLRequest(url: URL(string: "http://google.com")!)
     webView.loadRequest(req)
+  }
+
+  var selfCertError = false
+  var failedRequest: URLRequest?
+}
+
+extension MyWebViewController {
+
+  func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+    if selfCertError {
+      failedRequest = request
+      NSURLConnection(request: request, delegate: self)?.start()
+      return false
+    }
+    return true
+  }
+
+  func connection(_ connection: NSURLConnection,
+                  willSendRequestFor challenge: URLAuthenticationChallenge){
+    //server trust error
+    if challenge.protectionSpace.authenticationMethod == "NSURLAuthenticationMethodServerTrust" {
+      //set generated credential
+      challenge.sender?.use(URLCredential(trust: challenge.protectionSpace.serverTrust!), for: challenge)
+    }
+
+    challenge.sender?.continueWithoutCredential(for: challenge)
+  }
+
+  func connection(_ connection: NSURLConnection, didReceive data: Data) {
+    selfCertError = false
+    connection.cancel()
+    if let failedRequest = failedRequest {
+      //aftar connection establised successfully, load web view again
+      webView.loadRequest(failedRequest)
+    }
   }
 }
