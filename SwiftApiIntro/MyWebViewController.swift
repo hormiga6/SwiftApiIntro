e029283dfc2df24913bca8e15b6bfe838b386c21
Kotaro Arimura <kotaro_arimura@voyagegroup.com>
2017-06-07 15:23:00 +0900

    add webview

--- SwiftApiIntro/MyWebViewController.swift
+++ SwiftApiIntro/MyWebViewController.swift
@@ -2,8 +2,22 @@
 //  MyWebViewController.swift
 //  SwiftApiIntro
 //
-//  Created by 有村 皓太郎 on 2017/06/07.
 //
 //
 
-import Foundation
+import UIKit
+
+class MyWebViewController: UIViewController {
+
+  @IBOutlet weak var webView: UIWebView!
+
+  @IBAction func clickClose(_ sender: Any) {
+    self.dismiss(animated: true, completion: nil)
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    let req = URLRequest(url: URL(string: "https://google.com")!)
+    webView.loadRequest(req)
+  }
+}
